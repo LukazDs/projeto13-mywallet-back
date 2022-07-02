85265a85e5a896761905c924f58ee26f97fbb890
Lucas Duarte <lukazds2016@gmail.com>
2022-07-02 12:29:51 -0300

    Creating post requisition route for login screen

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from "cors";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
-//import dayjs from "dayjs";
+import bcrypt from 'bcrypt';
+import dayjs from "dayjs";
+import { v4 as uuid } from 'uuid';
 
 dotenv.config();
 
@@ -19,35 +21,40 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-const userSchema = joi.object({
+const registerUserSchema = joi.object({
     name: joi.string().required(),
-    email: joi.string().required()
+    email: joi.string().required(),
+    password: joi.string().required()
 });
 
-server.post("/users", async (req, res) => {
+const loginSchema = joi.object({
+    email: joi.string().required(),
+    password: joi.string().required()
+});
+
+server.post("/sign-up", async (req, res) => {
 
     const user = req.body;
 
-    const validation = userSchema.validate(user, { abortEarly: true });
+    const validation = registerUserSchema.validate(user, { abortEarly: true });
 
     if (validation.error) {
-        res.status(422).send("Email ou senha invalido!");
+        res.status(422).send("Email ou senha invalidos!");
         return;
     }
 
     try {
 
         const users = await db.collection('users').find().toArray();
+        const passwordHash = bcrypt.hashSync(user.password, 10);
 
         if (users.some(v => (v.name === user.name || v.email === user.email))) {
             res.status(409).send("Email ou nome já utilizado!");
             return;
         }
 
-        await db.collection("users").insertOne({ ...user, userId: new ObjectId()});
-
+        await db.collection("users").insertOne({ ...user, password: passwordHash });
         console.log(users)
-
         res.sendStatus(201);
 
     } catch (error) {
@@ -56,7 +63,36 @@ server.post("/users", async (req, res) => {
 
 })
 
+server.post("/sign-in", async (req, res) => {
 
+    const user = req.body;
+
+    const validation = loginSchema.validate(user);
+
+    if (validation.error) {
+        return res.status(422).send("Email ou senha invalidos!");
+    }
+
+    try {
+        const userDb = await db.collection('users').findOne({ email: user.email });
+
+        if (userDb && bcrypt.compareSync(user.password, userDb.password)) {
+            const token = uuid();
+
+            await db.collection('sessions').insertOne({
+                token,
+                userId: user._id
+            });
+
+            return res.status(201).send({ token });
+        } else {
+            return res.status(401).send('Senha ou email incorretos!');
+        }
+    } catch (error) {
+        res.status(500).send("Problema de cadastramento!");
+    }
+
+});
 
 server.listen(process.env.PORT, () => {
     console.log("Conexão estabelecida");
