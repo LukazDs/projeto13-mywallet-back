8e3007eb35dbf822bc35c1020071363ea57f472a
Lucas Duarte <lukazds2016@gmail.com>
2022-07-02 11:23:10 -0300

    Creating post requisition route for registration screen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
-//import joi from "joi";
+import joi from "joi";
 //import dayjs from "dayjs";
 
 dotenv.config();
@@ -19,6 +19,45 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required()
+});
+
+server.post("/users", async (req, res) => {
+
+    const user = req.body;
+
+    const validation = userSchema.validate(user, { abortEarly: true });
+
+    if (validation.error) {
+        res.status(422).send("Email ou senha invalido!");
+        return;
+    }
+
+    try {
+
+        const users = await db.collection('users').find().toArray();
+
+        if (users.some(v => (v.name === user.name || v.email === user.email))) {
+            res.status(409).send("Email ou nome já utilizado!");
+            return;
+        }
+
+        await db.collection("users").insertOne({ ...user, userId: new ObjectId()});
+
+        console.log(users)
+
+        res.sendStatus(201);
+
+    } catch (error) {
+        res.status(500).send("Problema de cadastramento!");
+    }
+
+})
+
+
+
 server.listen(process.env.PORT, () => {
-    console.log("Conexão estabelecida")
+    console.log("Conexão estabelecida");
 })
