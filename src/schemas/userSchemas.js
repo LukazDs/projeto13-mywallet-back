e240e17ccabc888f7de01be778857a90b8c75ba9
Lucas Duarte <lukazds2016@gmail.com>
2022-07-12 11:57:42 -0300

    Correcting email registration

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -1,19 +1,39 @@
 import joi from "joi";
 
 export const registerUserSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.string().required(),
-    password: joi.string().required(),
-    confirmPassword: joi.string().required()
-
+    name: joi
+        .string()
+        .required(),
+    email: joi
+        .string()
+        .pattern(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i)
+        .required(),
+    password: joi
+        .string()
+        .pattern(/[a-zA-Z0-9]{4,8}/)
+        .required(),
+    confirmPassword: joi
+        .string()
+        .pattern(/[a-zA-Z0-9]{4,8}/)
+        .required()
 });
 
 export const loginSchema = joi.object({
-    email: joi.string().required(),
-    password: joi.string().required()
+    email: joi
+        .string()
+        .pattern(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i)
+        .required(),
+    password: joi
+        .string()
+        .pattern(/[a-zA-Z0-9]{4,8}/)
+        .required()
 });
 
 export const postSchema = joi.object({
-    value: joi.number().required(),
-    description: joi.string().required()
+    value: joi
+        .number()
+        .required(),
+    description: joi
+        .string()
+        .required()
 });
